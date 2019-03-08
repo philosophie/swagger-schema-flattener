7bd284670b610fd46f7761fe79d20b911bc28f31
Ashwin Vaswani <ashwinv11@gmail.com>
2019-03-08 14:22:07 -0800

    OAS3 Support (#1)
    * add top level support
    
    * add support for requestBody

--- src/interfaces/swagger-param-flattener-extension.ts
+++ src/interfaces/swagger-param-flattener-extension.ts
@@ -1,4 +1,13 @@
+import { ParameterObject, ISpecificationExtension } from 'openapi3-ts'
+
+export interface CustomRequestBodyObject extends ISpecificationExtension {
+  description?: string
+  required?: boolean
+}
+
 export interface SwaggerParamFlattenerExtension {
   realPath: string
   displayPath: string
+  isTopLevel: boolean
+  topLevelProps?: ParameterObject
 }
